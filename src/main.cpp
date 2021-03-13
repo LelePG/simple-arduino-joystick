d031d1171b585058328a42840cc5eb351467b91c
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-03-13 14:19:22 -0300

    programação do módulo de joystick finalizado

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
+#include "Keyboard.h"
 
 #define eixoX 9
 #define eixoY 10
+
+void movimentaX(int leitura);
+void movimentaY(int leitura);
 
 void setup() {
   
@@ -16,22 +20,34 @@ void loop() {
   leituraX = map(analogRead(eixoX), 0, 1023, -1, 2);
   leituraY = map(analogRead(eixoY), 0, 1023, -1, 2);
 
-  //Movimentação em X
-  if(leituraX < 0){
-    Serial.println("direita");
-  }
-  else if (leituraX > 0){
-    Serial.println("Esquerda");
-  }
+  movimentaX(leituraX);
+  movimentaY(leituraY);
+  delay(50);
 
-  //Movimentação em Y
-  if(leituraY < 0){
-    Serial.println("cima");
-  }
-  else if (leituraY > 0){
-    Serial.println("Baixo");
-  }
-  
-  delay(100);
 }
 
+void movimentaX(int leitura){
+  if(leitura < 0){
+    Keyboard.press(KEY_RIGHT_ARROW);
+  }
+  else if (leitura > 0){
+    Keyboard.press(KEY_LEFT_ARROW);
+  }
+  else{
+    Keyboard.release(KEY_RIGHT_ARROW);
+    Keyboard.release(KEY_LEFT_ARROW);
+  }
+}
+
+void movimentaY(int leitura){
+ if(leitura < 0){
+    Keyboard.press(KEY_UP_ARROW);
+  }
+  else if (leitura > 0){
+    Keyboard.press(KEY_DOWN_ARROW);
+  }
+  else{
+    Keyboard.release(KEY_UP_ARROW);
+    Keyboard.release(KEY_DOWN_ARROW);
+  }
+}
